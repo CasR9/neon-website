157d0f005bb78c831bf20459ec2355719585df6b
CasR9 <casrovers@gmail.com>
2023-05-07 19:50:50 +0200

    Animated linear gradient background

--- src/index.php
+++ src/index.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./styles/stylesheet.css" rel="stylesheet" type="text/css">
     <title>Neon theme</title>
 </head>
 <body>
@@ -27,5 +28,7 @@
             </a>
         </div>
     </header>
+<br>
+<br>
 </body>
 </html>
