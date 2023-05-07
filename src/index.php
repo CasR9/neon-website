1673886523cf02f0f44ff7d432cb8a2f82ae9b6b
CasR9 <casrovers@gmail.com>
2023-05-07 19:04:59 +0200

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Author: Cas Rovers
+* Date:   7-5-2023
+* File:   index.php
+**/
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Neon theme</title>
+</head>
+<body>
+    <header>
+        <div>
+            Logo
+        </div>
+        <div>
+            <a href="#">
+                Lorum
+            </a>
+            <a href="#">
+                Ipsum
+            </a>
+        </div>
+    </header>
+</body>
+</html>
